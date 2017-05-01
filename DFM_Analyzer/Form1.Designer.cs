1b79a3fc845936bd642b7148097ddd9070596333
GEROMAX <tobita0112@gmail.com>
2017-05-01 14:53:18 +0900

    First time commit.

---
+++ DFM_Analyzer/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace DFM_Analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileSelect1 = new System.Windows.Forms.Button();
+            this.txtFilePath1 = new System.Windows.Forms.TextBox();
+            this.txtFilePath2 = new System.Windows.Forms.TextBox();
+            this.btnFileSelect2 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPropertyName = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.tv1 = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tv2 = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFileSelect1
+            // 
+            this.btnFileSelect1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFileSelect1.Location = new System.Drawing.Point(1097, 8);
+            this.btnFileSelect1.Name = "btnFileSelect1";
+            this.btnFileSelect1.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelect1.TabIndex = 0;
+            this.btnFileSelect1.Text = "選択1";
+            this.btnFileSelect1.UseVisualStyleBackColor = true;
+            this.btnFileSelect1.Click += new System.EventHandler(this.btnFileSelect1_Click);
+            // 
+            // txtFilePath1
+            // 
+            this.txtFilePath1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath1.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath1.Name = "txtFilePath1";
+            this.txtFilePath1.ReadOnly = true;
+            this.txtFilePath1.Size = new System.Drawing.Size(1079, 19);
+            this.txtFilePath1.TabIndex = 1;
+            // 
+            // txtFilePath2
+            // 
+            this.txtFilePath2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath2.Location = new System.Drawing.Point(12, 37);
+            this.txtFilePath2.Name = "txtFilePath2";
+            this.txtFilePath2.ReadOnly = true;
+            this.txtFilePath2.Size = new System.Drawing.Size(1079, 19);
+            this.txtFilePath2.TabIndex = 3;
+            // 
+            // btnFileSelect2
+            // 
+            this.btnFileSelect2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFileSelect2.Location = new System.Drawing.Point(1097, 33);
+            this.btnFileSelect2.Name = "btnFileSelect2";
+            this.btnFileSelect2.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelect2.TabIndex = 2;
+            this.btnFileSelect2.Text = "選択2";
+            this.btnFileSelect2.UseVisualStyleBackColor = true;
+            this.btnFileSelect2.Click += new System.EventHandler(this.btnFileSelect2_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "DFM (*.dfm)|*.dfm";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "対象プロパティ";
+            // 
+            // txtPropertyName
+            // 
+            this.txtPropertyName.Location = new System.Drawing.Point(89, 70);
+            this.txtPropertyName.Name = "txtPropertyName";
+            this.txtPropertyName.Size = new System.Drawing.Size(273, 19);
+            this.txtPropertyName.TabIndex = 5;
+            this.txtPropertyName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPropertyName_KeyDown);
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(368, 68);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 6;
+            this.btnAnalyze.Text = "解析";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // tv1
+            // 
+            this.tv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tv1.Location = new System.Drawing.Point(12, 129);
+            this.tv1.Name = "tv1";
+            this.tv1.Size = new System.Drawing.Size(300, 621);
+            this.tv1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ファイル1";
+            // 
+            // tv2
+            // 
+            this.tv2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tv2.Location = new System.Drawing.Point(872, 129);
+            this.tv2.Name = "tv2";
+            this.tv2.Size = new System.Drawing.Size(300, 621);
+            this.tv2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(870, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ファイル2";
+            // 
+            // dgv
+            // 
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(318, 129);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 21;
+            this.dgv.Size = new System.Drawing.Size(548, 621);
+            this.dgv.TabIndex = 11;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(449, 68);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 12;
+            this.btnOutput.Text = "出力";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "txt|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 762);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tv2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tv1);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.txtPropertyName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilePath2);
+            this.Controls.Add(this.btnFileSelect2);
+            this.Controls.Add(this.txtFilePath1);
+            this.Controls.Add(this.btnFileSelect1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileSelect1;
+        private System.Windows.Forms.TextBox txtFilePath1;
+        private System.Windows.Forms.TextBox txtFilePath2;
+        private System.Windows.Forms.Button btnFileSelect2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPropertyName;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.TreeView tv1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView tv2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ DFM_Analyzer/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DFM_Analyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFileSelect1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK.Equals(this.openFileDialog.ShowDialog()))
+            {
+                this.txtFilePath1.Text = this.openFileDialog.FileName;
+            }
+        }
+
+        private void btnFileSelect2_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK.Equals(this.openFileDialog.ShowDialog()))
+            {
+                this.txtFilePath2.Text = this.openFileDialog.FileName;
+            }
+        }
+
+        private void txtPropertyName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keys.Enter.Equals(e.KeyCode))
+            {
+                this.btnAnalyze.PerformClick();
+            }
+        }
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.txtFilePath1.Text) ||
+                string.IsNullOrEmpty(this.txtFilePath2.Text) ||
+                string.IsNullOrEmpty(this.txtPropertyName.Text))
+            {
+                return;
+            }
+
+            var analyzer1 = new DfmAnalyzer();
+            var analyzer2 = new DfmAnalyzer();
+            try
+            {
+                analyzer1.Analyze(this.txtFilePath1.Text);
+                this.tv1.Nodes.Clear();
+                this.AddTree(this.tv1.Nodes, analyzer1.DfmObject);
+                this.tv1.ExpandAll();
+
+                analyzer2.Analyze(this.txtFilePath2.Text);
+                this.tv2.Nodes.Clear();
+                this.AddTree(this.tv2.Nodes, analyzer2.DfmObject);
+                this.tv2.ExpandAll();
+
+                this.dgv.DataSource = analyzer1.CreatePropertyCompareData(this.txtPropertyName.Text, analyzer2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void AddTree(TreeNodeCollection nodes, DfmInfomation dfmInfo)
+        {
+            var nowNode = nodes.Add(dfmInfo.ObjectClass + "：" + dfmInfo.ObjectName);
+            dfmInfo.ChildObjects.ForEach(info => this.AddTree(nowNode.Nodes, info));
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            if (null == this.dgv.DataSource)
+            {
+                return;
+            }
+            if (!DialogResult.OK.Equals(this.saveFileDialog.ShowDialog()))
+            {
+                return;
+            }
+
+            var data = (List<PropertyCompareData>)this.dgv.DataSource;
+            var sb = new StringBuilder();
+            sb.AppendLine("ファイル1：" + this.txtFilePath1.Text);
+            sb.AppendLine("ファイル2：" + this.txtFilePath2.Text);
+            sb.AppendLine("プロパティ：" + this.txtPropertyName.Text).AppendLine("");
+            sb.AppendLine(string.Join(",", new string[] { "オブジェクト名", "ファイル1", "ファイル2", "比較結果" }));
+            data.ForEach(item => sb.AppendLine(string.Join(",", new string[] { item.ObjectName, item.Value1, item.Value2, item.CompareResult })));
+            File.WriteAllText(this.saveFileDialog.FileName, sb.ToString(), Encoding.GetEncoding("Shift_JIS"));
+        }
+    }
+
+    public class DfmAnalyzer
+    {
+        public DfmInfomation DfmObject { get; private set; }
+
+        public DfmAnalyzer()
+        {
+        }
+
+        public void Analyze(string filePath)
+        {
+            this.DfmObject = new DfmInfomation(File.ReadAllText(filePath, Encoding.GetEncoding("Shift_JIS")));
+        }
+
+        public List<PropertyCompareData> CreatePropertyCompareData(string propertyName, DfmAnalyzer analyzer)
+        {
+            var list = new List<PropertyCompareData>();
+            this.addPropertyCompareData(propertyName, this.DfmObject, analyzer.DfmObject, list);
+            return list;
+        }
+
+        private void addPropertyCompareData(string propertyName, DfmInfomation dfmInfoA, DfmInfomation dfmInfoB, List<PropertyCompareData> list)
+        {
+            var compData = this.getPropertyCompareData(propertyName, dfmInfoA, dfmInfoB);
+            if (!list.Exists(match => match.GetHashCode().Equals(compData.GetHashCode())))
+            {
+                list.Add(compData);
+            }           
+
+            if (null != dfmInfoA)
+            {
+                foreach (DfmInfomation dfmInfo in dfmInfoA.ChildObjects)
+                {
+                    var infoB = (dfmInfoB != null) ? dfmInfoB.ChildObjects.Find(match => dfmInfo.ObjectName.Equals(match.ObjectName)) : null;
+                    this.addPropertyCompareData(propertyName, dfmInfo, infoB, list);
+                }
+            }
+            if (null != dfmInfoB)
+            {
+                foreach (DfmInfomation dfmInfo in dfmInfoB.ChildObjects)
+                {
+                    var infoA = (dfmInfoA != null) ? dfmInfoA.ChildObjects.Find(match => dfmInfo.ObjectName.Equals(match.ObjectName)) : null;
+                    this.addPropertyCompareData(propertyName, infoA, dfmInfo, list);
+                }
+            }
+        }
+
+        private PropertyCompareData getPropertyCompareData(string propertyName, DfmInfomation dfmInfoA, DfmInfomation dfmInfoB)
+        {
+            if (null == dfmInfoA)
+            {
+                return new PropertyCompareData(dfmInfoB.ObjectName, null, dfmInfoB[propertyName]);
+            }
+            else if (null == dfmInfoB)
+            {
+                return new PropertyCompareData(dfmInfoA.ObjectName, dfmInfoA[propertyName], null);
+            }
+            else
+            {
+                return new PropertyCompareData(dfmInfoA.ObjectName, dfmInfoA[propertyName], dfmInfoB[propertyName]);
+            }
+        }
+    }
+
+    public class PropertyCompareData
+    {
+        public string ObjectName { get; set; }
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+        public string CompareResult
+        {
+            get
+            {
+                if (null == this.Value1 && null == this.Value2)
+                {
+                    return "No Property";
+                }
+
+                return string.Compare(this.Value1, this.Value2).Equals(0) ? "OK" : "NG";
+            }
+        }
+
+        public PropertyCompareData(string name, string val1, string val2)
+        {
+            this.ObjectName = name;
+            this.Value1 = val1;
+            this.Value2 = val2;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ObjectName.GetHashCode();
+        }
+    }
+
+    public class DfmInfomation : IComparable<DfmInfomation>
+    {
+        public const string PREFIX = "object ";
+        public const string SUFIX = "end";
+        public const string INDENT = "  ";
+        public const string CRLF = "\r\n";
+
+        public string ObjectName { get; set; }
+        public string ObjectClass { get; set; }
+        public string ObjectValue { get; set; }
+        public string this[string propertyName]
+        {
+            get
+            {
+                return this.getPropertyValue(propertyName);
+            }
+        }
+        public List<DfmInfomation> ChildObjects { get; set; }
+
+        public DfmInfomation(string objValue)
+        {
+            if (!objValue.StartsWith(PREFIX))
+            {
+                throw new Exception("解析失敗");
+            }
+
+            var lines = objValue.Split(CRLF.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var objInfo = lines[0].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            this.ObjectName = objInfo[0].TrimStart((PREFIX + " ").ToCharArray());
+            this.ObjectClass = objInfo[1].TrimStart(" ".ToCharArray()).TrimEnd(CRLF.ToCharArray());
+
+            var sb = new StringBuilder();
+            foreach (string line in lines)
+            {
+                if (line.StartsWith(PREFIX) || line.Equals(SUFIX))
+                {
+                    continue;
+                }
+                sb.AppendLine(line.StartsWith(INDENT) ? line.Substring(2) : line);
+            }
+            this.ObjectValue = sb.ToString();
+            
+            var childFind = false;
+            this.ChildObjects = new List<DfmInfomation>();
+            var childLines = this.ObjectValue.Split(CRLF.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in childLines)
+            {
+                if (!childFind && line.StartsWith(PREFIX))
+                {
+                    childFind = true;
+                    sb.Clear().AppendLine(line);                    
+                }
+                else if (childFind && line.Equals(SUFIX))
+                {
+                    childFind = false;
+                    sb.AppendLine(line);
+                    this.ChildObjects.Add(new DfmInfomation(sb.ToString()));
+                    sb.Clear();
+                }
+                else if (childFind)
+                {
+                    sb.AppendLine(line);
+                }
+            }
+            this.ChildObjects.Sort();
+        }
+
+        private string getPropertyValue(string propertyName)
+        {
+            var lines = this.ObjectValue.Split(CRLF.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var sb = new StringBuilder();
+            var find = false;
+            foreach (string line in lines)
+            {
+                if (find && line.StartsWith(INDENT))
+                {
+                    sb.Append(line.TrimStart(INDENT.ToCharArray()).TrimEnd(CRLF.ToCharArray()));
+                    continue;
+                }
+                else if (find)
+                {
+                    break;
+                }
+
+                if (!line.StartsWith(propertyName))
+                {
+                    continue;
+                }
+                else
+                {
+                    //見つけた
+                    find = true;
+                    //保持
+                    sb.Append(line.TrimEnd(CRLF.ToCharArray()));
+                    continue;
+                }
+            }
+            if (sb.Length <= 0)
+            {
+                return null;
+            }
+
+            return sb.ToString().Split(" = ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1];
+        }
+
+        int IComparable<DfmInfomation>.CompareTo(DfmInfomation other)
+        {
+            var thisValue = this.ObjectClass + this.ObjectName;
+            var otherValue = other.ObjectClass + other.ObjectName;
+            return thisValue.CompareTo(otherValue);
+        }
+    }
+}
